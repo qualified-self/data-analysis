1d3a574e1ec334df3700ed372cd737a6e0fd745b
Tats <info@sofianaudry.com>
2016-03-30 17:30:00 +0200

    Allow conversion of plot to PNG or PDF file instead of showing.

--- view_plot.py
+++ view_plot.py
@@ -2,7 +2,19 @@ import matplotlib.pyplot as plt
 import pickle
 import sys
 
-filename = sys.argv[1]
-pickle.load(file(filename))
-plt.show()
+import json, argparse
 
+# Parse commandline arguments.
+parser = argparse.ArgumentParser()
+parser.add_argument("plot_file", type=str, help="The file containing the plot (saved in pickled format)")
+parser.add_argument("-o", "--output", type=str, default=None, help="Optional output file")
+
+args = parser.parse_args()
+
+filename = args.plot_file
+pickle.load(file(filename))
+
+if (args.output == None):
+  plt.show()
+else:
+  plt.savefig(args.output)
